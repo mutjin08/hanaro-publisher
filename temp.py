a6a5f798eda93805a196db13cb37869c1757839f
mutjin08 <mutjin08@gmail.com>
2024-01-25 17:41:51 +0900

    24-01-25

--- temp.py
+++ temp.py
@@ -1,29 +1,39 @@
-from collections import deque
+"""
+전체 학생의 수 n
+체육복을 도난당한 학생들의 번호가 담긴 배열 lost
+여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve
 
-def bfs(maps, x1, y1, x2, y2):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+체육수업을 들을 수 있는 학생의 최댓값을 return
+"""
+def solution(n, lost, reserve):
+    answer = 0
+    states = [1]*(n+1)
     
+    for i in lost:
+        states[i]-=1
+    for i in reserve:
+        states[i]+=1
     
-    n, m = len(maps), len(maps[0])
-    visited = [[0 for _ in range(m)] for _ in range(n)]
+    if states[1]==0 and states[2]==2:
+        states[2]-=1
+        states[1]+=1
     
-    q = deque([[[x1, y1, 0]]])
-    visited[x1][y1] = 1
+    for i in range(2, n):
+        if states[i]!=0:
+            continue
+        
+        if states[i-1]==2:
+            states[i-1]-=1
+            states[i]+=1
+            continue
+        
+        if states[i+1]==2:
+            states[i+1]-=1
+            states[i]+=1
+            continue
     
-    while q:
-        x, y, cnt = q.popleft()
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0<=nx<n and 0<=ny<m and not visited[nx][ny]:
-            if nx==x2 and ny==y2:
-                return cnt+1
-            q.append([nx, ny, cnt+1])
-            visited[nx][ny] = 1
-    if not visited[x2][y2]:
-        return -1
-    
+    if states[n]==0 and states[n-1]==2:
+        states[n-1]-=1
+        states[n]+=1
 
-def solution(maps):
-    n, m = len(maps), len(maps[0])
-    return bfs(maps, 0, 0, n-1, m-1)
+    return n - states.count(0)
