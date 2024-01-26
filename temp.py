a296a63f7ee343a3801d77fd2b9722bbf9ab45ab
mutjin08 <mutjin08@gmail.com>
2024-01-26 11:41:06 +0900

    24-01-26

--- temp.py
+++ temp.py
@@ -1,10 +1,11 @@
-def oneInArray(arr, target, x):
-    for i in range(len(arr)):
-        if arr[i]+x == target:
-            return i
-    return -1
+def twoArray(arr1, arr2, target):
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+            if arr1[i]+arr2[j]==target:
+                return i, j
+    return -1, -1
 
-def twoInArray(arr, target):
+def oneArray(arr, target):
     for i in range(len(arr)):
         for j in range(i+1, len(arr)):
             if arr[i]+arr[j]==target:
@@ -18,15 +19,11 @@ def solution(coin, cards):
     stage = 1
     
     while True:
-        print(haves, cards)
         if len(cards)<2:
             return stage
         
-        pick1, pick2 = cards[0], cards[1]
-        cards = cards[2:]
-        
         #1
-        i, j = twoInArray(haves, n+1)
+        i, j = oneArray(haves, n+1)
         if i!=-1 and j!=-1:
             haves = haves[:i]+haves[i+1:j]+haves[j+1:]
             stage += 1
@@ -34,27 +31,25 @@ def solution(coin, cards):
             
         #2-1
         if coin>=1:
-            i = oneInArray(haves, n+1, pick1)
-            if i!=-1:
+            i, j = twoArray(haves, cards, n+1)
+            if i!=-1 and j!=-1:
                 haves = haves[:i]+haves[i+1:]
-                coin -=1
-                stage += 1
-                continue
-        
-        #2-2
-        if coin>=1:
-            i = oneInArray(haves, n+1, pick2)
-            if i!=-1:
-                haves = haves[:i]+haves[i+1:]
+                if j<=1:
+                  cards = cards[2:]
+                elif j>=2:
+                  cards = cards[:j]+cards[j+1:]
+                  cards = cards[2:]
                 coin -=1
                 stage += 1
                 continue
                 
         #3
         if coin>=2:
-            if pick1+pick2==n+1:
+            i, j = oneArray(cards, n+1)
+            if i!=-1 and j!=-1:
+                cards = cards[:i]+cards[i+1:j]+cards[j+1:]
                 coin-=2
-                stage+=1
+                stage += 1
                 continue
                 
         return stage
