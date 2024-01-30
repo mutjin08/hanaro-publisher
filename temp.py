517348e30d387f7e9aa7e3985188d89141a17e4e
mutjin08 <mutjin08@gmail.com>
2024-01-30 11:13:28 +0900

    24-01-30

--- temp.py
+++
@@ -1,7 +0,0 @@
-from itertools import combinations_with_replacement
-n = 3
-for case in combinations_with_replacement(range(11), n):
-  lion = [0 for _ in range(11)]
-  for i in case:
-    lion[10-i] += 1
-  print(lion)
